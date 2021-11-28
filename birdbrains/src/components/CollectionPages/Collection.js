d9427afe029d2631cfb7fddaa8096ba7b2e8cbc8
stricklandrbls <strickland.rbls@gmail.com>
2021-11-27 18:38:13 -0600

    Added wallet connect functionality

--- birdbrains/src/components/CollectionPages/Collection.js
+++ birdbrains/src/components/CollectionPages/Collection.js
@@ -15,9 +15,13 @@ function Collection(){
     const params = useParams();
     const [walletDataObj, updateWalletData] = useState(walletData);
 
-    const initMetaMask = () =>{
+    const initMetaMask = async () =>{
         if(window.ethereum?.isMetaMask){
-
+            const accounts = await window.ethereum.enable();
+            const address = await window.ethereum.request({ method: "eth_requestAccounts" });
+            const network = await window.ethereum.request({ method: "net_version" });
+            console.log(accounts[0], address[0], network);
+            updateWalletData({ ...walletDataObj, account: address[0], network: network });
         }
         else{
             updateWalletData({...walletDataObj, error: "No MetaMask"});
@@ -31,9 +35,25 @@ function Collection(){
         <div class="collection-banner w3-display-container w3-center w3-padding ">
             <br></br><br></br>
             <h1 class="">{params.collectionName}</h1>
-            <a class="w3-button w3-border">Connect Wallet</a>
+            <div class="w3-border w3-round">
+                <WalletStatus data={walletDataObj} />
+            </div>
         </div>
     )
 }
 
+function WalletStatus(props){
+
+    if (props.data.error = null){
+        return( <div class="w3-border w3-round w3-padding w3-red">Can't Connect...</div> )
+    }
+    else{
+        let address = "";
+        const address_len = props.data.account.length;
+
+        address = props.data.account.substring(0, 6) + "..." + props.data.account.substring(address_len - 6, address_len);
+
+        return ( <div class="w3-border w3-round w3-padding w3-green">{address}</div> )
+    }
+}
 export default Collection;
