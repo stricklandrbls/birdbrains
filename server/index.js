28de997e9d6af5811cf80bd78a9201b41d9eb9ca
stricklandrbls <strickland.rbls@gmail.com>
2021-11-30 20:00:45 -0600

    Added a Procfile

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const path      = require("path");
 const PORT      = process.env.PORT || 3001;
 const app       = express();
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-app.get("*", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-})
+// app.get("*", (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// })
 
 app.get("/api", (req, res) =>{
     res.json( {message: "Hello from server!"} );
