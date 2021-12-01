3eed4831eed3d7435ed1233b47002bac141aa77d
stricklandrbls <strickland.rbls@gmail.com>
2021-12-01 13:19:34 -0600

    removed debug statement.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) =>{
-    console.log("Got api call");
     res.json( {message: "Hello from server!"} );
 });
 
