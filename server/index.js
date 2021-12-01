d177697012094780bac49c74dc52e40678798d54
stricklandrbls <strickland.rbls@gmail.com>
2021-12-01 14:38:00 -0600

    finalizing server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
-const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express   = require("express");
+const path      = require("path");
+
+const PORT      = process.env.PORT || 3001;
+const app       = express();
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+})
 
 app.get("/api", (req, res) =>{
     res.json( {message: "Hello from server!"} );
